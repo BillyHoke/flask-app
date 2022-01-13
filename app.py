0a46222cc143151a1dccd97a12e326d2be005f2d
Stefan <stefan.fenech@live.com.au>
2022-01-13 19:14:25 +1100

    Added basic API integration + functionality

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 import os
+from flask.scaffold import _matching_loader_thinks_module_is_package
 import psycopg2
+import requests
+from requests import api
 
 DB_URL = os.environ.get("DATABASE_URL", "dbname=project_2")
 SECRET_KEY = os.environ.get("SECRET_KEY", "MY_SECRET_KEY")
@@ -9,13 +12,24 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
-    conn = psycopg2.connect(DB_URL)
-    cur = conn.cursor()
-    cur.execute('SELECT 1', [])  # Query to check that the DB connected
-    conn.close()
-    return 'Hello, world!'
+    i = 1
+    pokemon = []
+    while i <= 151:
+        api_result = f'https://pokeapi.co/api/v2/pokemon/{i}'
+        pokemon_data = requests.get(api_result).json()
+        pokemon.append(pokemon_data)
+        i += 1
+    return render_template('index.html', pokemon=pokemon)
+
+
+@app.route('/search', methods=["GET"])
+def search():
+    search_query = request.values.get('query')
+    api_result = f'https://pokeapi.co/api/v2/pokemon/{search_query}'
+    pokemon_data = requests.get(api_result).json()
+    return render_template('pokemon.html', search_query=api_result, pokemon_image=pokemon_data["sprites"]["other"]["dream_world"]["front_default"], pokemon_name=pokemon_data["name"])
 
 
 if __name__ == "__main__":
